fe49983c60b58340818dd603b254c0f2015bd62a
robdcampbell <rdeancampbell@gmail.com>
2023-04-02 17:39:19 -0400

    Navbar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Toolkit</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
