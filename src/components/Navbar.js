f0ec10ba069120ecd38f4928f83054aa2befe145
robdcampbell <rdeancampbell@gmail.com>
2023-04-03 21:53:22 -0400

    Continue Creating Cart Redux Slices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,24 @@ import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  // // Log the
+  //   console.log(
+  //     useSelector((store) => {
+  //       console.log(store);
+  //     })
+  //   );
+
+  const { amount } = useSelector((store) => store.cart);
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux Toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
       </div>
     </nav>
   );
--- src/store.js
+++ src/store.js
@@ -5,6 +5,6 @@ import authReducer from "./features/auth/authTest";
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
-    auth: authReducer,
+    // auth: authReducer,
   },
 });
